c9a42a2de570dfc2a771fa80b3f55fce6bfb4349
taochang wu <taochang@TogeMacPro.local>
2017-09-04 23:04:58 +0800

    add fun

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const color = require('colors')
 exports.printMsg = function() {
 	console.log("This message is from the npm-package-demo11 module".red)
 }
+
+export function printPath() {
+	let path = __dirname
+	console.log(`current path is：${path}`.blue)
+}
