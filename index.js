5b878bad98ed112a26268957aebad9c717f391ab
taochang wu <taochang@TogeMacPro.local>
2017-09-04 23:15:59 +0800

    del

--- index.js
+++
@@ -1,9 +0,0 @@
-const color = require('colors')
-exports.printMsg = function() {
-	console.log("This message is from the npm-package-demo11 module".red)
-}
-
-exports function printPath() {
-	let path = __dirname
-	console.log(`current path is：${path}`.blue)
-}
