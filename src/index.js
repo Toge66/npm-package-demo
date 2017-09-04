e4a14d4f0dc58eb8f47905b4218710afc461b66c
taochang wu <taochang@TogeMacPro.local>
2017-09-04 23:12:38 +0800

    fix

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const color = require('colors')
+exports.printMsg = function() {
+	console.log("This message is from the npm-package-demo11 module".red)
+}
+
+exports.printPath = function() {
+	let path = __dirname
+	console.log(`current path is：${path}`.blue)
+}
